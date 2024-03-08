67ab091f15f888d7fe3d700656ee36d9dc5c6c1e
Olesia2805 <vysotina2002@gmail.com>
2024-03-08 12:54:47 +0100

    Snowflake

--- KochSnowflake.py
+++ KochSnowflake.py
@@ -10,15 +10,18 @@ def koch_curve(t, order, size_):
 
 def draw_koch_curve(order, size = 300):
     window = turtle.Screen()
-    window.bgcolor("white")
+    window.bgcolor("black")
 
     t = turtle.Turtle()
+    t.color("white")
     t.speed(0)
     t.penup()
-    t.goto(-size / 2, 0)
+    t.goto(-size / 2, size / 3)
     t.pendown()
 
-    koch_curve(t, order, size)
+    for _ in range(3):
+        koch_curve(t, order, size)
+        t.right(120)
 
     window.mainloop()
 
